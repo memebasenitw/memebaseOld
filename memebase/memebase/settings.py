6883c2fff9004577a7f76d0856340c87948bde8d
Rushabh Yapuram <rushabh.yapuram@gmail.com>
2018-03-24 18:34:49 +0530

    Add meme-generator module and index page

--- memebase/memebase/settings.py
+++ memebase/memebase/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'memebase.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, '../')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -101,3 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "../static-assets")
+]
